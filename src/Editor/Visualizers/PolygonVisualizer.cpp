7002857ea54f87502091ff6c8737858622c1e18f
Niklas Damberg <niklas.damberg@gmail.com>
2018-10-22 21:12:59 +0200

    Remove unused code.

--- src/Editor/Visualizers/PolygonVisualizer.cpp
+++ src/Editor/Visualizers/PolygonVisualizer.cpp
@@ -8,49 +8,6 @@
 
 using namespace editor;
 
-PolygonVisualizer::PolygonVisualizer(const std::vector<Polygon>& polygons)
-    : m_polygons(polygons)
-{
-    m_texture = mono::CreateTexture("res/textures/placeholder.png");
-}
-
-void PolygonVisualizer::doDraw(mono::IRenderer& renderer) const
-{
-    for(const Polygon& polygon : m_polygons)
-    {
-        constexpr mono::Color::RGBA line_color(1.0f, 0.0f, 0.7f);
-        constexpr mono::Color::RGBA point_color(0.0f, 1.0f, 0.7f);
-        constexpr mono::Color::RGBA selected_color(0.8f, 0.8f, 0.8f);
-
-        std::vector<unsigned short> indices;
-        for(size_t index = 0; index < polygon.vertices.size(); ++index)
-            indices.push_back(index);
-
-        renderer.DrawGeometry(polygon.vertices, polygon.texture_coordinates, indices, m_texture);
-
-        renderer.DrawClosedPolyline(polygon.vertices, line_color, 2.0f);
-        renderer.DrawPoints(polygon.vertices, point_color, 3.0f);
-
-        renderer.DrawPoints({ polygon.centroid }, point_color, 4.0f);
-
-        /*
-        if(m_selected)
-        {
-            const math::Quad& bb = LocalBoundingBox();
-            renderer.DrawQuad(bb, selected_color, 1.0f);
-        }
-         */
-    }
-}
-
-math::Quad PolygonVisualizer::BoundingBox() const
-{
-    return math::InfQuad;
-}
-
-
-
-
 PolygonVisualizer2::PolygonVisualizer2(const std::vector<math::Vector>& points, const math::Vector& mouse_position)
     : m_points(points),
       m_mousePosition(mouse_position)
--- src/Editor/Visualizers/PolygonVisualizer.h
+++ src/Editor/Visualizers/PolygonVisualizer.h
@@ -8,27 +8,6 @@
 
 namespace editor
 {
-    struct Polygon
-    {
-        std::vector<math::Vector> vertices;
-        std::vector<math::Vector> texture_coordinates;
-        math::Vector centroid;
-        float texture_repeate;
-    };
-    
-    class PolygonVisualizer : public mono::IDrawable
-    {
-    public:
-
-        PolygonVisualizer(const std::vector<Polygon>& polygons);
-
-        virtual void doDraw(mono::IRenderer& renderer) const;
-        virtual math::Quad BoundingBox() const;
-
-        const std::vector<Polygon>& m_polygons;
-        mono::ITexturePtr m_texture;
-    };
-
     class PolygonVisualizer2 : public mono::IDrawable
     {
     public:
