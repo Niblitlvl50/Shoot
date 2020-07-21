8f83dabd024d598ec4930f32ed81445d6465c32d
Niklas Damberg <niklas.damberg@gmail.com>
2020-07-21 22:36:27 +0200

    Removed unused code.

--- src/Game/Spawner.h
+++ src/Game/Spawner.h
@@ -2,52 +2,13 @@
 #pragma once
 
 #include "IGameSystem.h"
-#include "System/System.h"
 #include "Math/Vector.h"
 #include "MonoFwd.h"
 
-#include <memory>
 #include <vector>
-#include <string>
 
 namespace game
 {
-    struct SpawnPoint
-    {
-        float radius = 1.0f;
-        math::Vector position;
-    };
-
-    struct Wave
-    {
-        std::string name;
-        std::vector<std::string> tags;
-    };
-
-    std::vector<Wave> LoadWaveFile(const char* wave_file);
-
-    class Spawner
-    {
-    public:
-
-        Spawner(
-            const std::vector<SpawnPoint>& spawn_points, const std::vector<Wave>& waves, mono::EventHandler& event_handler);
-        void CheckForSpawn();
-        void EntityDestroyed(unsigned int spawn_id);
-
-    private:
-
-        void SpawnNextWave();
-
-        std::vector<SpawnPoint> m_spawn_points;
-        std::vector<Wave> m_waves;
-        mono::EventHandler& m_event_handler;
-        std::unique_ptr<System::ITimer> m_timer;
-
-        int m_wave_index;
-        std::vector<unsigned int> m_current_spawned_ids;
-    };
-
     class SpawnSystem : public mono::IGameSystem
     {
     public:
