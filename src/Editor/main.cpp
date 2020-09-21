a0e236f9039c6dec127a59f42a79604d7ce8ac3a
Niklas Damberg <niklas.damberg@gmail.com>
2020-09-21 21:58:59 +0200

    Remove reference so world.world.

--- src/Editor/main.cpp
+++ src/Editor/main.cpp
@@ -25,7 +25,9 @@ int main(int argc, const char* argv[])
     // This is assumed to be the file argument
     const char* file_name = (argc < 2) ? nullptr : argv[1];
     if(!file_name)
-        file_name = "res/world.world";
+    {
+        return -1;
+    }
 
     constexpr size_t max_entities = 500;
 
