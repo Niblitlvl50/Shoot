89b5311ba5ce081f1e26a4be7028a6af3de3cd0a
Niklas Damberg <niklas.damberg@gmail.com>
2020-11-19 14:10:38 +0100

    State changes happens in update if needed.

--- src/Game/StateMachine.h
+++ src/Game/StateMachine.h
@@ -31,11 +31,7 @@ public:
 
     void TransitionTo(StateId new_state)
     {
-        m_active_state = new_state;
-
-        State& state = m_states.at(new_state);
-        if(state.enter_state)
-            state.enter_state();
+        m_wanted_state = new_state;
     }
 
     StateId ActiveState() const
@@ -45,6 +41,15 @@ public:
 
     void UpdateState(const UpdateContext& argument)
     {
+        if(m_active_state != m_wanted_state)
+        {
+            State& state = m_states.at(m_wanted_state);
+            if(state.enter_state)
+                state.enter_state();
+
+            m_active_state = m_wanted_state;
+        }
+
         State& state = m_states.at(m_active_state);
         if(state.update_state)
             state.update_state(argument);
@@ -53,5 +58,6 @@ public:
 private:
 
     StateId m_active_state{};
+    StateId m_wanted_state{};
     std::unordered_map<StateId, State> m_states;
 };
