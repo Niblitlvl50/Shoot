4f9cf3777a8db18bf652e31423ba5830ce1b52db
Niblitlvl50 <niklas.damberg@gmail.com>
2021-01-01 23:21:20 +0100

    Fixed the network launch scripts.

--- scripts/run_client.sh
+++ scripts/run_client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./bin/game_exe.app/Contents/MacOS/game_exe --position 720 0 --size 720 450 --zone 3 &> right_game.log &
+./bin/game_exe.app/Contents/MacOS/game_exe --position 720 0 --size 700 700 --zone 3 &> right_game.log &
 wait
