dc844fdde11796fa341161524b35887964542cb2
Niblitlvl50 <niklas.damberg@gmail.com>
2021-03-06 14:14:50 +0100

    Fixed static background so it adhear to pixels per meter.

--- src/Game/World/StaticBackground.cpp
+++ src/Game/World/StaticBackground.cpp
@@ -6,10 +6,13 @@
 #include "Rendering/RenderSystem.h"
 #include "Rendering/RenderBuffer/BufferFactory.h"
 #include "Rendering/IRenderer.h"
+#include "Rendering/Texture/ITexture.h"
 #include "Rendering/Texture/ITextureFactory.h"
 
 StaticBackground::StaticBackground(const char* background_texture)
 {
+    m_texture = mono::GetTextureFactory()->CreateTexture(background_texture);
+
     constexpr math::Quad background_bb = math::Quad(-100.0f, -100.0f, 100.0f, 100.0f);
 
     const math::Vector vertices[] = {
@@ -31,13 +34,14 @@ StaticBackground::StaticBackground(const char* background_texture)
         math::ZeroVec,
     };
 
-    //const math::Vector& repeate = (background_bb.mB - background_bb.mA) / 8.0f;
-    const math::Vector repeate(12.0f, 12.0f);
-        
+    const float pixels_per_meter = mono::PixelsPerMeter();
+    const float background_width = math::Width(background_bb);
+    const float background_height = math::Height(background_bb);
+    const math::Vector repeate =
+        math::Vector(background_width / m_texture->Width(), background_height / m_texture->Height()) * pixels_per_meter;
+
     for(size_t index = 0; index < std::size(vertices); ++index)
         uv_coordinates[index] = math::MapVectorInQuad(vertices[index], background_bb) * repeate;
-
-    m_texture = mono::GetTextureFactory()->CreateTexture(background_texture);
 
     m_vertex_buffer = mono::CreateRenderBuffer(mono::BufferType::STATIC, mono::BufferData::FLOAT, 2, std::size(vertices), vertices);
     m_texture_buffer = mono::CreateRenderBuffer(mono::BufferType::STATIC, mono::BufferData::FLOAT, 2, std::size(uv_coordinates), uv_coordinates);
