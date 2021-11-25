9b657c32feefff8ea18a2e1b06d16abe07b1a844
Niblitlvl50 <niklas.damberg@gmail.com>
2021-11-25 23:05:40 +0100

    Fix homing behaviour to be frame rate independant.

--- src/Game/Behaviour/HomingBehaviour.cpp
+++ src/Game/Behaviour/HomingBehaviour.cpp
@@ -68,7 +68,7 @@ HomingResult HomingBehaviour::Run(const mono::UpdateContext& update_context)
     m_current_heading += turn_value;
 
     const math::Vector angle1 = math::VectorFromAngle(m_current_heading);
-    m_body->ApplyLocalImpulse(angle1 * m_forward_velocity, math::ZeroVec);
+    m_body->ApplyLocalImpulse(angle1 * m_forward_velocity * update_context.delta_s, math::ZeroVec);
 
     return { m_current_heading, math::DistanceBetween(m_target_position, entity_position) };
 }
