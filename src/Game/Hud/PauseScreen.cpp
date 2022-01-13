9dee6587cad20e9a29523ab82b8955d0ed4d38aa
Niblitlvl50 <niklas.damberg@gmail.com>
2022-01-13 20:23:46 +0100

    Fixed some pause screen alignment.

--- src/Game/Hud/PauseScreen.cpp
+++ src/Game/Hud/PauseScreen.cpp
@@ -2,6 +2,7 @@
 #include "PauseScreen.h"
 
 #include "Rendering/RenderSystem.h"
+#include "Rendering/Text/TextFunctions.h"
 #include "FontIds.h"
 
 using namespace game;
@@ -15,20 +16,22 @@ PauseScreen::PauseScreen()
     const float background_x = (m_width - background_width) / 2.0f;
     const float background_y = (m_height - background_height) / 2.0f;
 
-    UISquareElement* background_element = new UISquareElement(background_width, background_height, mono::Color::BLACK);
+    UISquareElement* background_element = new UISquareElement(background_width, background_height, mono::Color::BLACK, mono::Color::GRAY, 1.0f);
     background_element->SetPosition(background_x, background_y);
 
     UITextElement* pause_text =
         new UITextElement(shared::FontId::RUSSOONE_MEDIUM, "Pause", mono::FontCentering::DEFAULT_CENTER, mono::Color::OFF_WHITE);
-    pause_text->SetPosition(50.0f, background_height - 50.0f);
+    pause_text->SetPosition(background_x + 50.0f, background_y + background_height - 50.0f);
+
+    const math::Vector text_size = mono::MeasureString(shared::FontId::RUSSOONE_MEDIUM, "start to exit");
 
     UITextElement* exit_text =
         new UITextElement(shared::FontId::RUSSOONE_MEDIUM, "start to exit", mono::FontCentering::DEFAULT_CENTER, mono::Color::GRAY);
-    exit_text->SetPosition(50.0f, 0.0f);
+    exit_text->SetPosition(background_x + background_width - text_size.x - 50.0f, background_y + 30.0f);
 
     UITextureElement* texture_element = new UITextureElement("res/textures/gamepad/gamepad_button_layout.png");
     texture_element->SetScale(0.75f);
-    texture_element->SetPosition(background_width / 2.0f, background_height / 2.0f);
+    texture_element->SetPosition(m_width / 2.0f, m_height / 2.0f);
 
     // background_element->AddChild(pause_text);
     // background_element->AddChild(exit_text);
