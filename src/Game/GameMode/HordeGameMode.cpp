4f1f95df97436b61535ae3a58b87953b3a648c2e
Niblitlvl50 <niklas.damberg@gmail.com>
2023-04-01 21:07:58 +0200

    More horde mode stuff.

--- src/Game/GameMode/HordeGameMode.cpp
+++ src/Game/GameMode/HordeGameMode.cpp
@@ -1,25 +1,68 @@
 
 #include "HordeGameMode.h"
 
+#include "Events/GameEvents.h"
+#include "Events/GameEventFuncFwd.h"
+#include "GameCamera/CameraSystem.h"
+#include "Hud/BigTextScreen.h"
 #include "Hud/LevelTimerUIElement.h"
+#include "Hud/PauseScreen.h"
 #include "Hud/PlayerUIElement.h"
 #include "Player/PlayerDaemonSystem.h"
 #include "Player/PlayerAuxiliaryDrawer.h"
 #include "RenderLayers.h"
-#include "TransformSystem/TransformSystem.h"
+#include "TriggerSystem/TriggerSystem.h"
 #include "Weapons/WeaponSystem.h"
 #include "WorldFile.h"
+#include "Zones/ZoneFlow.h"
 
+
+#include "Events/QuitEvent.h"
+#include "Events/PauseEvent.h"
+#include "Events/EventFuncFwd.h"
+#include "EventHandler/EventHandler.h"
+
+#include "Camera/ICamera.h"
+#include "Math/EasingFunctions.h"
 #include "EntitySystem/IEntityManager.h"
+#include "Rendering/RenderSystem.h"
 #include "Rendering/Sprite/SpriteSystem.h"
 #include "SystemContext.h"
+#include "TransformSystem/TransformSystem.h"
 #include "Zone/IZone.h"
+
+#include "System/Hash.h"
+
+namespace
+{
+    const uint32_t level_completed_hash = hash::Hash("level_completed");
+    const uint32_t level_gameover_hash = hash::Hash("level_gameover");
+    const uint32_t level_aborted_hash = hash::Hash("level_aborted");
+
+    const uint32_t show_shop_screen_hash = hash::Hash("show_shop_screen");
+}
+
+namespace tweak_values
+{
+    constexpr float level_result_duration_s = 3.0f;
+    constexpr float fade_duration_s = 1.0f;
+}
 
 using namespace game;
 
-
 HordeGameMode::HordeGameMode()
-{ }
+{
+    const GameModeStateMachine::StateTable state_table = {
+        GameModeStateMachine::MakeState(GameModeStates::FADE_IN, &HordeGameMode::ToFadeIn, &HordeGameMode::FadeIn, this),
+        GameModeStateMachine::MakeState(GameModeStates::RUN_GAME_MODE, &HordeGameMode::ToRunGameMode, &HordeGameMode::RunGameMode, this),
+        GameModeStateMachine::MakeState(GameModeStates::PACKAGE_DESTROYED, &HordeGameMode::ToPackageDestroyed, &HordeGameMode::LevelCompleted, this),
+        GameModeStateMachine::MakeState(GameModeStates::LEVEL_ABORTED, &HordeGameMode::ToLevelAborted, &HordeGameMode::LevelCompleted, this),
+        GameModeStateMachine::MakeState(GameModeStates::LEVEL_COMPLETED, &HordeGameMode::ToLevelCompleted, &HordeGameMode::LevelCompleted, this),
+        GameModeStateMachine::MakeState(GameModeStates::PAUSED, &HordeGameMode::ToPaused, &HordeGameMode::Paused, &HordeGameMode::ExitPaused, this),
+        GameModeStateMachine::MakeState(GameModeStates::FADE_OUT, &HordeGameMode::ToFadeOut, &HordeGameMode::FadeOut, this),
+    };
+    m_states.SetStateTableAndState(state_table, GameModeStates::FADE_IN);
+}
 
 HordeGameMode::~HordeGameMode() = default;
 
@@ -30,11 +73,20 @@ void HordeGameMode::Begin(
     mono::EventHandler* event_handler,
     const LevelMetadata& level_metadata)
 {
+    m_event_handler = event_handler;
+
     m_transform_system = system_context->GetSystem<mono::TransformSystem>();
+    m_render_system = system_context->GetSystem<mono::RenderSystem>();
+    m_trigger_system = system_context->GetSystem<game::TriggerSystem>();
     m_sprite_system = system_context->GetSystem<mono::SpriteSystem>();
     m_entity_manager = system_context->GetSystem<mono::IEntityManager>();
-    
-    WeaponSystem* weapon_system = system_context->GetSystem<game::WeaponSystem>();
+    m_camera_system = system_context->GetSystem<game::CameraSystem>();
+
+    mono::InputSystem* input_system = system_context->GetSystem<mono::InputSystem>();
+    game::WeaponSystem* weapon_system = system_context->GetSystem<game::WeaponSystem>();
+    game::UISystem* ui_system = system_context->GetSystem<game::UISystem>();
+
+    SetupEvents();
 
     // Player
     m_player_system = system_context->GetSystem<PlayerDaemonSystem>();
@@ -55,7 +107,23 @@ void HordeGameMode::Begin(
     m_timer_screen = std::make_unique<LevelTimerUIElement>();
     m_timer_screen->SetSeconds(m_level_timer);
 
+    // UI
+    m_big_text_screen = std::make_unique<BigTextScreen>(
+        level_metadata.level_name.c_str(),
+        level_metadata.level_description.c_str(),
+        mono::Color::RGBA(0.0f, 0.0f, 0.0f, 0.0f),
+        mono::Color::RGBA(0.0f, 0.0f, 0.0f, 0.0f),
+        mono::Color::GOLDEN_YELLOW,
+        mono::Color::GRAY,
+        BigTextScreen::TEXT | BigTextScreen::SUBTEXT);
+    m_big_text_screen->Hide();
+
+    m_pause_screen = std::make_unique<PauseScreen>(m_transform_system, input_system, m_entity_manager, event_handler, ui_system);
+    m_pause_screen->Hide();
+
     zone->AddUpdatableDrawable(m_player_ui.get(), LayerId::UI);
+    zone->AddUpdatableDrawable(m_big_text_screen.get(), LayerId::UI);
+    zone->AddUpdatableDrawable(m_pause_screen.get(), LayerId::UI);
     zone->AddUpdatableDrawable(m_timer_screen.get(), LayerId::UI);
 
     // Package
@@ -66,11 +134,19 @@ void HordeGameMode::Begin(
 int HordeGameMode::End(mono::IZone* zone)
 {
     zone->RemoveDrawable(m_package_aux_drawer.get());
+    zone->RemoveUpdatableDrawable(m_big_text_screen.get());
+    zone->RemoveUpdatableDrawable(m_pause_screen.get());
     zone->RemoveUpdatableDrawable(m_player_ui.get());
     zone->RemoveUpdatableDrawable(m_timer_screen.get());
 
     if(m_package_entity_id != mono::INVALID_ID)
         m_entity_manager->RemoveReleaseCallback(m_package_entity_id, m_package_release_callback);
+
+    m_event_handler->RemoveListener(m_gameover_token);
+    m_trigger_system->RemoveTriggerCallback(level_completed_hash, m_level_completed_trigger, mono::INVALID_ID);
+    m_trigger_system->RemoveTriggerCallback(level_gameover_hash, m_level_gameover_trigger, mono::INVALID_ID);
+    m_trigger_system->RemoveTriggerCallback(level_aborted_hash, m_level_aborted_trigger, mono::INVALID_ID);
+    m_trigger_system->RemoveTriggerCallback(show_shop_screen_hash, m_show_shop_screen_trigger, mono::INVALID_ID);
 
     return 0;
 }
@@ -79,6 +155,45 @@ void HordeGameMode::Update(const mono::UpdateContext& update_context)
 {
     m_level_timer += update_context.delta_s;
     m_timer_screen->SetSeconds(m_level_timer);
+
+    m_states.UpdateState(update_context);
+}
+
+void HordeGameMode::SetupEvents()
+{
+    // Quit and game over events
+    const GameOverFunc on_game_over = [this](const game::GameOverEvent& game_over_event) {
+        m_states.TransitionTo(GameModeStates::PACKAGE_DESTROYED);
+        return mono::EventResult::PASS_ON;
+    };
+    m_gameover_token = m_event_handler->AddListener(on_game_over);
+
+    const event::PauseEventFunc on_pause = [this](const event::PauseEvent& pause_event) {
+        const GameModeStates previous_state = m_states.PreviousState();
+        const GameModeStates new_state = pause_event.pause ? GameModeStates::PAUSED : previous_state;
+        m_states.TransitionTo(new_state);
+
+        return mono::EventResult::PASS_ON;
+    };
+    m_pause_token = m_event_handler->AddListener(on_pause);
+
+    const TriggerCallback level_event_callback = [this](uint32_t trigger_id) {
+        if(trigger_id == level_completed_hash)
+            m_states.TransitionTo(GameModeStates::LEVEL_COMPLETED);
+        else if(trigger_id == level_gameover_hash)
+            m_states.TransitionTo(GameModeStates::PACKAGE_DESTROYED);
+        else if(trigger_id == level_aborted_hash)
+            m_states.TransitionTo(GameModeStates::LEVEL_ABORTED);
+        else if(trigger_id == show_shop_screen_hash)
+        {
+            //const mono::ICamera* camera = m_camera_system->GetActiveCamera();
+            //m_shop_screen->ShowAt(camera->GetTargetPosition());
+        }
+    };
+    m_level_completed_trigger = m_trigger_system->RegisterTriggerCallback(level_completed_hash, level_event_callback, mono::INVALID_ID);
+    m_level_gameover_trigger = m_trigger_system->RegisterTriggerCallback(level_gameover_hash, level_event_callback, mono::INVALID_ID);
+    m_level_aborted_trigger = m_trigger_system->RegisterTriggerCallback(level_aborted_hash, level_event_callback, mono::INVALID_ID);
+    m_show_shop_screen_trigger = m_trigger_system->RegisterTriggerCallback(show_shop_screen_hash, level_event_callback, mono::INVALID_ID);
 }
 
 void HordeGameMode::OnSpawnPlayer(uint32_t player_entity_id, const math::Vector& position)
@@ -128,4 +243,98 @@ void HordeGameMode::SpawnPackage(const math::Vector& position)
     m_package_release_callback = m_entity_manager->AddReleaseCallback(m_package_entity_id, release_callback);
 
     System::Log("HordeGameMode|Spawning package[id:%u] at position %.2f %.2f", m_package_entity_id, position.x, position.y);
+}
+
+void HordeGameMode::ToFadeIn()
+{
+    m_fade_timer = 0.0f; 
+    m_render_system->TriggerScreenFade(mono::ScreenFadeState::FADE_IN, 1.0f, 0.0f);
+    m_big_text_screen->Show();
+}
+
+void HordeGameMode::FadeIn(const mono::UpdateContext& update_context)
+{
+    if(m_fade_timer < tweak_values::fade_duration_s)
+    {
+        const float alpha = math::EaseInCubic(m_fade_timer, tweak_values::fade_duration_s, 0.0f, 1.0f);
+        m_big_text_screen->SetAlpha(alpha);
+    }
+    else if(m_fade_timer < tweak_values::fade_duration_s + 3.0f)
+    {
+        const float alpha = math::EaseInCubic(m_fade_timer - tweak_values::fade_duration_s, 3.0f, 1.0f, -1.0f);
+        m_big_text_screen->SetAlpha(alpha);
+    }
+    else
+    {
+        m_big_text_screen->Hide();
+        m_states.TransitionTo(GameModeStates::RUN_GAME_MODE);
+    }
+
+    m_fade_timer += update_context.delta_s;
+}
+
+void HordeGameMode::ToRunGameMode()
+{}
+
+void HordeGameMode::RunGameMode(const mono::UpdateContext& update_context)
+{}
+
+void HordeGameMode::ToPackageDestroyed()
+{}
+
+void HordeGameMode::ToTimeout()
+{}
+
+void HordeGameMode::ToLevelCompleted()
+{}
+
+void HordeGameMode::ToLevelAborted()
+{
+    m_next_zone = game::ZoneResult::ZR_ABORTED;
+
+    m_pause_screen->Hide();
+
+    m_big_text_screen->SetText("Horde Aborted!");
+    m_big_text_screen->SetSubText("See you!");
+    m_big_text_screen->SetAlpha(0.0f);
+    m_big_text_screen->Show();
+
+    m_fade_timer = 0.0f;
+}
+
+void HordeGameMode::LevelCompleted(const mono::UpdateContext& update_context)
+{
+    const float alpha = math::EaseInCubic(m_fade_timer, 1.0f, 0.0f, 1.0f);
+    m_big_text_screen->SetAlpha(alpha);
+
+    m_fade_timer += update_context.delta_s;
+    if(m_fade_timer >= tweak_values::level_result_duration_s)
+        m_states.TransitionTo(GameModeStates::FADE_OUT);
+}
+
+void HordeGameMode::ToPaused()
+{
+    const mono::ICamera* camera = m_camera_system->GetActiveCamera();
+    m_pause_screen->ShowAt(camera->GetTargetPosition());
+}
+void HordeGameMode::Paused(const mono::UpdateContext& update_context)
+{ }
+void HordeGameMode::ExitPaused()
+{
+    m_pause_screen->Hide();
+}
+
+void HordeGameMode::ToFadeOut()
+{
+    m_fade_timer = 0.0f;
+    m_render_system->TriggerScreenFade(mono::ScreenFadeState::FADE_OUT, tweak_values::fade_duration_s, 0.0f);
+}
+void HordeGameMode::FadeOut(const mono::UpdateContext& update_context)
+{
+    const float alpha = math::EaseOutCubic(m_fade_timer, tweak_values::fade_duration_s, 1.0f, -1.0f);
+    m_big_text_screen->SetAlpha(alpha);
+
+    if(m_fade_timer > tweak_values::fade_duration_s)
+        m_event_handler->DispatchEvent(event::QuitEvent());
+    m_fade_timer += update_context.delta_s;
 }
--- src/Game/GameMode/HordeGameMode.h
+++ src/Game/GameMode/HordeGameMode.h
@@ -3,7 +3,11 @@
 
 #include "IGameMode.h"
 #include "MonoFwd.h"
+#include "EventHandler/EventToken.h"
 #include "Math/Vector.h"
+
+#include "Events/EventFwd.h"
+#include "StateMachine.h"
 
 #include <memory>
 
@@ -25,27 +29,76 @@ namespace game
         int End(mono::IZone* zone) override;
         void Update(const mono::UpdateContext& update_context) override;
 
+        void SetupEvents();
         void OnSpawnPlayer(uint32_t player_entity_id, const math::Vector& position);
         void SpawnPackage(const math::Vector& position);
 
+        void ToFadeIn();
+        void FadeIn(const mono::UpdateContext& update_context);
+
+        void ToRunGameMode();
+        void RunGameMode(const mono::UpdateContext& update_context);
+
+        void ToPackageDestroyed();
+        void ToTimeout();
+        void ToLevelCompleted();
+        void ToLevelAborted();
+        void LevelCompleted(const mono::UpdateContext& update_context);
+
+        void ToPaused();
+        void Paused(const mono::UpdateContext& update_context);
+        void ExitPaused();
+
+        void ToFadeOut();
+        void FadeOut(const mono::UpdateContext& update_context);
+
+        enum class GameModeStates
+        {
+            FADE_IN,
+            RUN_GAME_MODE,
+            PACKAGE_DESTROYED,
+            LEVEL_COMPLETED,
+            LEVEL_ABORTED,
+            PAUSED,
+            FADE_OUT
+        };
+
+        using GameModeStateMachine = StateMachine<GameModeStates, const mono::UpdateContext&>;
+        GameModeStateMachine m_states;
+
+        mono::EventHandler* m_event_handler;
+        class TriggerSystem* m_trigger_system;
+
+        mono::RenderSystem* m_render_system;
         mono::TransformSystem* m_transform_system;
         mono::SpriteSystem* m_sprite_system;
         mono::IEntityManager* m_entity_manager;
         class PlayerDaemonSystem* m_player_system;
+        class CameraSystem* m_camera_system;
+
+        mono::EventToken<struct GameOverEvent> m_gameover_token;
+        mono::EventToken<event::PauseEvent> m_pause_token;
 
         std::unique_ptr<class PackageAuxiliaryDrawer> m_package_aux_drawer;
         std::unique_ptr<class PlayerUIElement> m_player_ui;
         std::unique_ptr<class LevelTimerUIElement> m_timer_screen;
 
+        std::unique_ptr<class BigTextScreen> m_big_text_screen;
+        std::unique_ptr<class PauseScreen> m_pause_screen;
 
         bool m_spawn_package;
         bool m_package_spawned;
         math::Vector m_package_spawn_position;
         int m_next_zone;
 
+        uint32_t m_level_completed_trigger;
+        uint32_t m_level_gameover_trigger;
+        uint32_t m_level_aborted_trigger;
+        uint32_t m_show_shop_screen_trigger;
         uint32_t m_package_release_callback;
         uint32_t m_package_entity_id;
 
+        float m_fade_timer;
         float m_level_timer;
     };
 }
