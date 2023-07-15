005829a8e03360928e3c32c92b0ab5445599a97b
niblitlvl50 <niklas.damberg@gmail.com>
2023-07-15 21:24:08 +0200

    Fix uninitialized variable.

--- src/Game/GameMode/HordeGameMode.cpp
+++ src/Game/GameMode/HordeGameMode.cpp
@@ -64,6 +64,7 @@ HordeGameMode::HordeGameMode()
     : m_spawn_wave_timer(tweak_values::spawn_wave_interval_s - 5.0f)
     , m_wave_index(0)
     , m_loot_box_index(0)
+    , m_package_spawned(false)
 {
     const GameModeStateMachine::StateTable state_table = {
         GameModeStateMachine::MakeState(GameModeStates::FADE_IN, &HordeGameMode::ToFadeIn, &HordeGameMode::FadeIn, this),
