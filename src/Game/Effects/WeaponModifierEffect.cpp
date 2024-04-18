e038cbe38efafc2aae176ec4b5f370859c3ab638
niblitlvl50 <niklas.damberg@gmail.com>
2024-04-18 22:11:24 +0200

    Tweaked weapon modifier effect

--- src/Game/Effects/WeaponModifierEffect.cpp
+++ src/Game/Effects/WeaponModifierEffect.cpp
@@ -19,23 +19,30 @@ namespace
     void TrailGenerator(
         const mono::ParticleGeneratorContext& context, mono::ParticlePoolComponentView& component_view, const math::Vector& world_position)
     {
-        const float life = mono::Random(0.5f, 2.0f);
-        const float radians = mono::Random(0.0f, math::PI() * 2.0f);
-        const math::Vector offset = math::VectorFromAngle(radians) * mono::Random(0.0f, 0.1f);
+        const float life = mono::Random(0.3f, 1.0f);
+        const float radians = mono::Random(0.0f, math::TAU());
+        const math::Vector offset = math::VectorFromAngle(radians) * mono::Random(0.0f, 0.2f);
 
         component_view.position = world_position + offset;
-        component_view.velocity = math::Vector(0.0f, -0.75f);
+        component_view.velocity = math::Vector(0.0f, -0.1f);
         component_view.rotation = 0.0f;
         component_view.angular_velocity = 0.0f;
         
         using namespace mono::Color;
         component_view.gradient = mono::Color::MakeGradient<4>(
-            { 0.0f, 0.2f, 0.5f, 1.0f },
-            { mono::Color::MakeWithAlpha(WHITE, 0.0f), mono::Color::MakeWithAlpha(WHITE, 1.0f), mono::Color::MakeWithAlpha(WHITE, 0.5f), mono::Color::MakeWithAlpha(WHITE, 0.0f) }
+            {
+                0.0f, 0.2f, 0.5f, 1.0f
+            },
+            {
+                mono::Color::MakeWithAlpha(DARK_GRAY, 0.0f),
+                mono::Color::MakeWithAlpha(DARK_GRAY, 0.5f),
+                mono::Color::MakeWithAlpha(DARK_GRAY, 0.3f),
+                mono::Color::MakeWithAlpha(DARK_GRAY, 0.0f)
+            }
         );
-        component_view.size = 24.0f;
-        component_view.start_size = 24.0f;
-        component_view.end_size = 38.0f;
+        component_view.size = 16.0f;
+        component_view.start_size = 16.0f;
+        component_view.end_size = 32.0f;
 
         component_view.life = life;
         component_view.start_life = life;
@@ -53,12 +60,12 @@ WeaponModifierEffect::WeaponModifierEffect(
     mono::Entity particle_entity = m_entity_system->CreateEntity("WeaponModifierEffect", { TRANSFORM_COMPONENT, PARTICLE_SYSTEM_COMPONENT });
     m_particle_system->SetPoolData(
         particle_entity.id,
-        20,
+        50,
         "res/textures/particles/glyph3.png",
         mono::BlendMode::SOURCE_ALPHA,
-        mono::ParticleDrawLayer::PRE_GAMEOBJECTS,
+        mono::ParticleDrawLayer::POST_GAMEOBJECTS,
         mono::ParticleTransformSpace::WORLD,
-        0.025f,
+        0.03f,
         mono::DefaultUpdater);
 
     m_particle_entity = particle_entity.id;
@@ -88,7 +95,7 @@ void WeaponModifierEffect::EmitForDuration(float duration_s)
         m_particle_entity,
         math::ZeroVec,
         duration_s,
-        3.0f,
+        10.0f,
         mono::EmitterType::CONTINOUS,
         mono::EmitterMode::AUTO_ACTIVATED,
         generator_proxy);
