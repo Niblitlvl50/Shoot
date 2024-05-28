b0977e998d0a1ea79240960d461ac1a60ad20f28
niblitlvl50 <niklas.damberg@gmail.com>
2024-05-28 13:26:19 +0200

    Fix dragger size

--- src/Editor/Editor.cpp
+++ src/Editor/Editor.cpp
@@ -867,7 +867,7 @@ void Editor::UpdateGrabbers()
 float Editor::GetPickingDistance() const
 {
     const System::Size& size = m_window->Size();
-    return math::Width(m_camera->GetViewport()) / size.width * 5.0f;
+    return math::Width(m_camera->GetViewport()) / size.width * 10.0f;
 }
 
 void Editor::OnDeleteObject()
