79565084c85f110920961168965bcec935778813
niblitlvl50 <niklas.damberg@gmail.com>
2026-09-23 22:15:44 +0200

    Added fade in to train game mode.

--- src/Game/GameMode/TrainGameMode.cpp
+++ src/Game/GameMode/TrainGameMode.cpp
@@ -11,11 +11,18 @@
 #include "Events/QuitEvent.h"
 #include "Input/InputSystem.h"
 #include "Paths/PathSystem.h"
+#include "Rendering/RenderSystem.h"
+#include "Rendering/IRenderer.h"
 #include "SystemContext.h"
 
 #include "EntitySystem/IEntityManager.h"
 #include "System/Hash.h"
 #include "System/System.h"
+
+namespace tweak_values
+{
+    constexpr float fade_duration_s = 2.0f;
+}
 
 using namespace game;
 
@@ -29,10 +36,11 @@ void TrainGameMode::Begin(
     m_input_system = system_context->GetSystem<mono::InputSystem>();
     m_trigger_system = system_context->GetSystem<mono::TriggerSystem>();
     m_path_follower_system = system_context->GetSystem<game::PathFollowerSystem>();
+    m_render_system = system_context->GetSystem<mono::RenderSystem>();
     m_event_handler = event_handler;
 
-    //m_throttle_forward = false;
-    //m_throttle_backward = false;
+    renderer->SetScreenFadeAlpha(0.0f);
+    m_render_system->TriggerScreenFade(mono::ScreenFadeState::FADE_IN, tweak_values::fade_duration_s, 0.0f);
 
     // Player
     m_player_system = system_context->GetSystem<PlayerDaemonSystem>();
@@ -53,22 +61,6 @@ void TrainGameMode::Begin(
     };
     m_player_system->SpawnPlayersAt(level_metadata.player_spawn_point, player_spawned_cb);
 
-    /*
-    mono::IEntityManager* entity_manager = system_context->GetSystem<mono::IEntityManager>();
-    const std::vector<uint32_t> train_entities = entity_manager->CollectEntitiesWithTag(hash::Hash("train"));
-    m_train_entity_id = train_entities.empty() ? mono::INVALID_ID : train_entities.front();
-    
-    if(m_train_entity_id != mono::INVALID_ID)
-    {
-        game::CameraSystem* camera_system = system_context->GetSystem<game::CameraSystem>();
-        camera_system->FollowEntity(m_train_entity_id);
-    }
-    else
-    {
-        System::Log("TrainGameMode|No entity tagged 'train' found, unable to drive.");
-    }
-    */
-
     m_level_completed_hash = hash::Hash(level_metadata.completed_trigger.c_str());
     m_level_completed_alt_hash = hash::Hash(level_metadata.completed_alt_trigger.c_str());
     m_level_aborted_hash = hash::Hash(level_metadata.aborted_trigger.c_str());
@@ -88,12 +80,6 @@ void TrainGameMode::Begin(
     m_level_completed_alt_trigger = m_trigger_system->RegisterTriggerCallback(m_level_completed_alt_hash, level_hash_callback, mono::INVALID_ID);
     m_level_aborted_trigger = m_trigger_system->RegisterTriggerCallback(m_level_aborted_hash, level_hash_callback, mono::INVALID_ID);
     m_level_failed_trigger = m_trigger_system->RegisterTriggerCallback(m_level_failed_hash, level_hash_callback, mono::INVALID_ID);
-
-    /*
-    m_input_context = m_input_system->CreateContext(0, mono::InputContextBehaviour::ConsumeIfHandled, "TrainGameMode");
-    m_input_context->enabled = true;
-    m_input_context->keyboard_input = this;
-    */
 }
 
 int TrainGameMode::End(mono::IZone* zone)
@@ -103,86 +89,39 @@ int TrainGameMode::End(mono::IZone* zone)
     m_trigger_system->RemoveTriggerCallback(m_level_aborted_hash, m_level_aborted_trigger, mono::INVALID_ID);
     m_trigger_system->RemoveTriggerCallback(m_level_failed_hash, m_level_failed_trigger, mono::INVALID_ID);
 
-    //m_input_system->ReleaseContext(m_input_context);
     return m_game_mode_result;
 }
 
 void TrainGameMode::Update(const mono::UpdateContext& update_context)
 {
-    /*
-    if(m_train_entity_id == mono::INVALID_ID)
-    return;
-    
-    float throttle = 0.0f;
-    if(m_throttle_forward)
-    throttle += 1.0f;
-    if(m_throttle_backward)
-    throttle -= 1.0f;
-    
-    m_path_follower_system->SetThrottle(m_train_entity_id, throttle);
-    */
 }
 
 void TrainGameMode::Completed()
 {
-    m_game_mode_result = game::ZoneResult::ZR_COMPLETED;
-    m_event_handler->DispatchEvent(event::QuitEvent());
+    TriggerFadeOutAndQuit(game::ZoneResult::ZR_COMPLETED);
 }
 
 void TrainGameMode::CompletedAlt()
 {
-    m_game_mode_result = game::ZoneResult::ZR_COMPLETED_ALT;
-    m_event_handler->DispatchEvent(event::QuitEvent());
+    TriggerFadeOutAndQuit(game::ZoneResult::ZR_COMPLETED_ALT);
 }
 
 void TrainGameMode::GameOver()
 {
-    m_game_mode_result = game::ZoneResult::ZR_GAME_OVER;
-    m_event_handler->DispatchEvent(event::QuitEvent());
+    TriggerFadeOutAndQuit(game::ZoneResult::ZR_GAME_OVER);
 }
 
 void TrainGameMode::Aborted()
 {
-    m_game_mode_result = game::ZoneResult::ZR_ABORTED;
-    m_event_handler->DispatchEvent(event::QuitEvent());
+    TriggerFadeOutAndQuit(game::ZoneResult::ZR_ABORTED);
 }
 
-/*
-mono::InputResult TrainGameMode::KeyDown(const event::KeyDownEvent& event)
+void TrainGameMode::TriggerFadeOutAndQuit(int zone_result)
 {
-    switch(event.key)
-    {
-        case Keycode::W:
-        case Keycode::UP:
-        m_throttle_forward = true;
-        break;
-        case Keycode::S:
-        case Keycode::DOWN:
-        m_throttle_backward = true;
-        break;
-        default:
-        break;
-    }
-    
-    return mono::InputResult::Handled;
-}
+    m_game_mode_result = zone_result;
 
-mono::InputResult TrainGameMode::KeyUp(const event::KeyUpEvent& event)
-{
-    switch(event.key)
-    {
-        case Keycode::W:
-        case Keycode::UP:
-        m_throttle_forward = false;
-        break;
-        case Keycode::S:
-        case Keycode::DOWN:
-        m_throttle_backward = false;
-        break;
-        default:
-        break;
-    }
-    
-    return mono::InputResult::Handled;
+    const mono::ScreenFadeCallback on_fade_complete = [this](mono::ScreenFadeState state) {
+        m_event_handler->DispatchEvent(event::QuitEvent());
+    };
+    m_render_system->TriggerScreenFade(mono::ScreenFadeState::FADE_OUT, tweak_values::fade_duration_s, 0.0f, on_fade_complete);
 }
-*/
--- src/Game/GameMode/TrainGameMode.h
+++ src/Game/GameMode/TrainGameMode.h
@@ -3,7 +3,6 @@
 
 #include "MonoFwd.h"
 #include "IGameMode.h"
-#include "Input/InputInterfaces.h"
 
 #include <cstdint>
 
@@ -11,7 +10,7 @@ namespace game
 {
     // A minimal mode where the player drives a track-locked train (a "path_follower"
     // component with manual_control enabled) by throttling forward/backward. No combat.
-    class TrainGameMode : public IGameMode //, public mono::IKeyboardInput
+    class TrainGameMode : public IGameMode
     {
     public:
 
@@ -29,21 +28,18 @@ namespace game
         void GameOver();
         void Aborted();
 
-        //mono::InputResult KeyDown(const event::KeyDownEvent& event) override;
-        //mono::InputResult KeyUp(const event::KeyUpEvent& event) override;
-
     private:
+
+        void TriggerFadeOutAndQuit(int zone_result);
 
         mono::InputSystem* m_input_system;
         mono::EventHandler* m_event_handler;
         mono::TriggerSystem* m_trigger_system;
+        mono::RenderSystem* m_render_system;
         class PathFollowerSystem* m_path_follower_system;
         class PlayerDaemonSystem* m_player_system;
-        mono::InputContext* m_input_context;
 
         uint32_t m_train_entity_id;
-        //bool m_throttle_forward;
-        //bool m_throttle_backward;
 
         uint32_t m_level_completed_hash;
         uint32_t m_level_completed_alt_hash;
